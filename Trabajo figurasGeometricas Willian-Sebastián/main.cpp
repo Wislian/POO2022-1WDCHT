0281d57ea027f09f1ecda22cbebf929f6c107708
Sebastián Gómez Valencia <98537540+sgomez1205@users.noreply.github.com>
2022-03-20 15:02:40 -0500

    MD: ARCHIVOS CORRECCIÓN

--- Trabajo figurasGeometricas Willian-Sebastián/main.cpp
+++ Trabajo figurasGeometricas Willian-Sebastián/main.cpp
@@ -2,17 +2,49 @@
 // Created by Willian on 17/03/2022.
 //
 #include <iostream>
+#include "Sistema.h"
 
 using std::cout;
 using std::endl;
 
-void menu(){
+void menu(Sistema sistema){
+    int opcion = 0;
     cout<<"Bienvenido a nuestro programa - Willian Chapid & Sebastian Gomez "<<endl;
+    do {
+        cout << "MENU\n";
+        cout << "1) Agregar figura" << endl;
+        cout << "2) Dibujar Figuras" << endl;
+        cout << "3) Mostrar Perimetros" << endl;
+        cout << "4) Suma de Areas" << endl;
+        cout << "0) Salir\n";
+        cin >> opcion;
+        switch (opcion) {
+            case 1:
+                cout << "AGREGAR FIGURA\n";
+                sistema.agregarFigura();
+                break;
+            case 2:
+                cout << "FIGURAS DIBUJADAS\n";
+                sistema.dibujarFiguras();
+                break;
+            case 3:
+                cout << "PERIMETROS DE LAS FIGURAS\n";
+                sistema.mostrarPerimetros();
+                break;
+            case 4:
+                cout << "SUMA DE LAS AREAS\n";
+                sistema.sumarAreas();
+                break;
+            default: break;
 
+        }
+    }while(opcion != 0 );
+    cout << "Muchas gracias por usar nuestro sistema , vuelva pronto!!!\n";
 }
 
 int main(){
-    menu();
-
+    Sistema sistema;
+    menu(sistema);
+    return 0;
 }
 
